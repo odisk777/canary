352f5a846c68e8cf990fafe1ac49aad5dc64da79
Beats <daniel15042014@hotmail.com>
2023-05-08 21:30:33 -0400

    add try-catch

--- src/map/map.cpp
+++ src/map/map.cpp
@@ -582,183 +582,195 @@ const Tile* Map::canWalkTo(const Creature &creature, const Position &pos) const 
 }
 
 bool Map::getPathMatching(const Creature &creature, std::forward_list<Direction> &dirList, const FrozenPathingConditionCall &pathCondition, const FindPathParams &fpp) const {
-	Position start = creature.getPosition();
-	return getPathMatching(start, dirList, pathCondition, fpp);
+	try {
+		Position start = creature.getPosition();
+		return getPathMatching(start, dirList, pathCondition, fpp);
+	} catch (const std::invalid_argument &e) {
+		SPDLOG_ERROR("An error occurred in the '[{}]' function: {}", __FUNCTION__, e.what());
+	} catch (const std::exception &e) {
+		SPDLOG_ERROR("An unexpected error occurred in the '[{}]' function: {}", __FUNCTION__, e.what());
+	}
 }
 
 bool Map::getPathMatching(const Position &start, std::forward_list<Direction> &dirList, const FrozenPathingConditionCall &pathCondition, const FindPathParams &fpp) const {
-	Position pos = start;
-	Position endPos;
+	try {
+		Position pos = start;
+		Position endPos;
 
-	AStarNodes nodes(pos.x, pos.y);
+		AStarNodes nodes(pos.x, pos.y);
 
-	int32_t bestMatch = 0;
+		int32_t bestMatch = 0;
 
-	static constexpr int_fast32_t dirNeighbors[8][5][2] = {
-		{ { -1, 0 }, { 0, 1 }, { 1, 0 }, { 1, 1 }, { -1, 1 } },
-		{ { -1, 0 }, { 0, 1 }, { 0, -1 }, { -1, -1 }, { -1, 1 } },
-		{ { -1, 0 }, { 1, 0 }, { 0, -1 }, { -1, -1 }, { 1, -1 } },
-		{ { 0, 1 }, { 1, 0 }, { 0, -1 }, { 1, -1 }, { 1, 1 } },
-		{ { 1, 0 }, { 0, -1 }, { -1, -1 }, { 1, -1 }, { 1, 1 } },
-		{ { -1, 0 }, { 0, -1 }, { -1, -1 }, { 1, -1 }, { -1, 1 } },
-		{ { 0, 1 }, { 1, 0 }, { 1, -1 }, { 1, 1 }, { -1, 1 } },
-		{ { -1, 0 }, { 0, 1 }, { -1, -1 }, { 1, 1 }, { -1, 1 } }
-	};
+		static constexpr int_fast32_t dirNeighbors[8][5][2] = {
+			{ { -1, 0 }, { 0, 1 }, { 1, 0 }, { 1, 1 }, { -1, 1 } },
+			{ { -1, 0 }, { 0, 1 }, { 0, -1 }, { -1, -1 }, { -1, 1 } },
+			{ { -1, 0 }, { 1, 0 }, { 0, -1 }, { -1, -1 }, { 1, -1 } },
+			{ { 0, 1 }, { 1, 0 }, { 0, -1 }, { 1, -1 }, { 1, 1 } },
+			{ { 1, 0 }, { 0, -1 }, { -1, -1 }, { 1, -1 }, { 1, 1 } },
+			{ { -1, 0 }, { 0, -1 }, { -1, -1 }, { 1, -1 }, { -1, 1 } },
+			{ { 0, 1 }, { 1, 0 }, { 1, -1 }, { 1, 1 }, { -1, 1 } },
+			{ { -1, 0 }, { 0, 1 }, { -1, -1 }, { 1, 1 }, { -1, 1 } }
+		};
 
-	static constexpr int_fast32_t allNeighbors[8][2] = {
-		{ -1, 0 }, { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, -1 }, { 1, -1 }, { 1, 1 }, { -1, 1 }
-	};
+		static constexpr int_fast32_t allNeighbors[8][2] = {
+			{ -1, 0 }, { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, -1 }, { 1, -1 }, { 1, 1 }, { -1, 1 }
+		};
 
-	const Position startPos = pos;
+		const Position startPos = pos;
 
-	AStarNode* found = nullptr;
-	while (fpp.maxSearchDist != 0 || nodes.getClosedNodes() < 100) {
-		AStarNode* n = nodes.getBestNode();
-		if (!n) {
-			if (found) {
-				break;
+		AStarNode* found = nullptr;
+		while (fpp.maxSearchDist != 0 || nodes.getClosedNodes() < 100) {
+			AStarNode* n = nodes.getBestNode();
+			if (!n) {
+				if (found) {
+					break;
+				}
+				return false;
 			}
+
+			const int_fast32_t x = n->x;
+			const int_fast32_t y = n->y;
+			pos.x = x;
+			pos.y = y;
+			if (pathCondition(startPos, pos, fpp, bestMatch)) {
+				found = n;
+				endPos = pos;
+				if (bestMatch == 0) {
+					break;
+				}
+			}
+
+			uint_fast32_t dirCount;
+			const int_fast32_t* neighbors;
+			if (n->parent) {
+				const int_fast32_t offset_x = n->parent->x - x;
+				const int_fast32_t offset_y = n->parent->y - y;
+				if (offset_y == 0) {
+					if (offset_x == -1) {
+						neighbors = *dirNeighbors[DIRECTION_WEST];
+					} else {
+						neighbors = *dirNeighbors[DIRECTION_EAST];
+					}
+				} else if (!fpp.allowDiagonal || offset_x == 0) {
+					if (offset_y == -1) {
+						neighbors = *dirNeighbors[DIRECTION_NORTH];
+					} else {
+						neighbors = *dirNeighbors[DIRECTION_SOUTH];
+					}
+				} else if (offset_y == -1) {
+					if (offset_x == -1) {
+						neighbors = *dirNeighbors[DIRECTION_NORTHWEST];
+					} else {
+						neighbors = *dirNeighbors[DIRECTION_NORTHEAST];
+					}
+				} else if (offset_x == -1) {
+					neighbors = *dirNeighbors[DIRECTION_SOUTHWEST];
+				} else {
+					neighbors = *dirNeighbors[DIRECTION_SOUTHEAST];
+				}
+				dirCount = fpp.allowDiagonal ? 5 : 3;
+			} else {
+				dirCount = 8;
+				neighbors = *allNeighbors;
+			}
+
+			const int_fast32_t f = n->f;
+			for (uint_fast32_t i = 0; i < dirCount; ++i) {
+				pos.x = x + *neighbors++;
+				pos.y = y + *neighbors++;
+
+				if (fpp.maxSearchDist != 0 && (Position::getDistanceX(startPos, pos) > fpp.maxSearchDist || Position::getDistanceY(startPos, pos) > fpp.maxSearchDist)) {
+					continue;
+				}
+
+				if (fpp.keepDistance && !pathCondition.isInRange(startPos, pos, fpp)) {
+					continue;
+				}
+
+				const Tile* tile;
+				AStarNode* neighborNode = nodes.getNodeByPosition(pos.x, pos.y);
+				if (neighborNode) {
+					tile = getTile(pos.x, pos.y, pos.z);
+				} else {
+					tile = getTile(pos.x, pos.y, pos.z);
+					if (!tile || tile->hasFlag(TILESTATE_BLOCKSOLID)) {
+						continue;
+					}
+				}
+
+				// The cost (g) for this neighbor
+				const int_fast32_t cost = AStarNodes::getMapWalkCost(n, pos, true);
+				const int_fast32_t extraCost = 0;
+				const int_fast32_t newf = f + cost + extraCost;
+
+				if (neighborNode) {
+					if (neighborNode->f <= newf) {
+						// The node on the closed/open list is cheaper than this one
+						continue;
+					}
+
+					neighborNode->f = newf;
+					neighborNode->parent = n;
+					nodes.openNode(neighborNode);
+				} else {
+					// Does not exist in the open/closed list, create a new node
+					neighborNode = nodes.createOpenNode(n, pos.x, pos.y, newf);
+					if (!neighborNode) {
+						if (found) {
+							break;
+						}
+						return false;
+					}
+				}
+			}
+
+			nodes.closeNode(n);
+		}
+
+		if (!found) {
 			return false;
 		}
 
-		const int_fast32_t x = n->x;
-		const int_fast32_t y = n->y;
-		pos.x = x;
-		pos.y = y;
-		if (pathCondition(startPos, pos, fpp, bestMatch)) {
-			found = n;
-			endPos = pos;
-			if (bestMatch == 0) {
-				break;
-			}
-		}
-
-		uint_fast32_t dirCount;
-		const int_fast32_t* neighbors;
-		if (n->parent) {
-			const int_fast32_t offset_x = n->parent->x - x;
-			const int_fast32_t offset_y = n->parent->y - y;
-			if (offset_y == 0) {
-				if (offset_x == -1) {
-					neighbors = *dirNeighbors[DIRECTION_WEST];
-				} else {
-					neighbors = *dirNeighbors[DIRECTION_EAST];
-				}
-			} else if (!fpp.allowDiagonal || offset_x == 0) {
-				if (offset_y == -1) {
-					neighbors = *dirNeighbors[DIRECTION_NORTH];
-				} else {
-					neighbors = *dirNeighbors[DIRECTION_SOUTH];
-				}
-			} else if (offset_y == -1) {
-				if (offset_x == -1) {
-					neighbors = *dirNeighbors[DIRECTION_NORTHWEST];
-				} else {
-					neighbors = *dirNeighbors[DIRECTION_NORTHEAST];
-				}
-			} else if (offset_x == -1) {
-				neighbors = *dirNeighbors[DIRECTION_SOUTHWEST];
-			} else {
-				neighbors = *dirNeighbors[DIRECTION_SOUTHEAST];
-			}
-			dirCount = fpp.allowDiagonal ? 5 : 3;
-		} else {
-			dirCount = 8;
-			neighbors = *allNeighbors;
-		}
-
-		const int_fast32_t f = n->f;
-		for (uint_fast32_t i = 0; i < dirCount; ++i) {
-			pos.x = x + *neighbors++;
-			pos.y = y + *neighbors++;
-
-			if (fpp.maxSearchDist != 0 && (Position::getDistanceX(startPos, pos) > fpp.maxSearchDist || Position::getDistanceY(startPos, pos) > fpp.maxSearchDist)) {
-				continue;
-			}
-
-			if (fpp.keepDistance && !pathCondition.isInRange(startPos, pos, fpp)) {
-				continue;
-			}
-
-			const Tile* tile;
-			AStarNode* neighborNode = nodes.getNodeByPosition(pos.x, pos.y);
-			if (neighborNode) {
-				tile = getTile(pos.x, pos.y, pos.z);
-			} else {
-				tile = getTile(pos.x, pos.y, pos.z);
-				if (!tile || tile->hasFlag(TILESTATE_BLOCKSOLID)) {
-					continue;
-				}
-			}
-
-			// The cost (g) for this neighbor
-			const int_fast32_t cost = AStarNodes::getMapWalkCost(n, pos, true);
-			const int_fast32_t extraCost = 0;
-			const int_fast32_t newf = f + cost + extraCost;
-
-			if (neighborNode) {
-				if (neighborNode->f <= newf) {
-					// The node on the closed/open list is cheaper than this one
-					continue;
-				}
-
-				neighborNode->f = newf;
-				neighborNode->parent = n;
-				nodes.openNode(neighborNode);
-			} else {
-				// Does not exist in the open/closed list, create a new node
-				neighborNode = nodes.createOpenNode(n, pos.x, pos.y, newf);
-				if (!neighborNode) {
-					if (found) {
-						break;
-					}
-					return false;
-				}
-			}
-		}
-
-		nodes.closeNode(n);
-	}
-
-	if (!found) {
-		return false;
-	}
-
-	int_fast32_t prevx = endPos.x;
-	int_fast32_t prevy = endPos.y;
-
-	found = found->parent;
-	while (found) {
-		pos.x = found->x;
-		pos.y = found->y;
-
-		int_fast32_t dx = pos.getX() - prevx;
-		int_fast32_t dy = pos.getY() - prevy;
-
-		prevx = pos.x;
-		prevy = pos.y;
-
-		if (dx == 1 && dy == 1) {
-			dirList.push_front(DIRECTION_NORTHWEST);
-		} else if (dx == -1 && dy == 1) {
-			dirList.push_front(DIRECTION_NORTHEAST);
-		} else if (dx == 1 && dy == -1) {
-			dirList.push_front(DIRECTION_SOUTHWEST);
-		} else if (dx == -1 && dy == -1) {
-			dirList.push_front(DIRECTION_SOUTHEAST);
-		} else if (dx == 1) {
-			dirList.push_front(DIRECTION_WEST);
-		} else if (dx == -1) {
-			dirList.push_front(DIRECTION_EAST);
-		} else if (dy == 1) {
-			dirList.push_front(DIRECTION_NORTH);
-		} else if (dy == -1) {
-			dirList.push_front(DIRECTION_SOUTH);
-		}
+		int_fast32_t prevx = endPos.x;
+		int_fast32_t prevy = endPos.y;
 
 		found = found->parent;
+		while (found) {
+			pos.x = found->x;
+			pos.y = found->y;
+
+			int_fast32_t dx = pos.getX() - prevx;
+			int_fast32_t dy = pos.getY() - prevy;
+
+			prevx = pos.x;
+			prevy = pos.y;
+
+			if (dx == 1 && dy == 1) {
+				dirList.push_front(DIRECTION_NORTHWEST);
+			} else if (dx == -1 && dy == 1) {
+				dirList.push_front(DIRECTION_NORTHEAST);
+			} else if (dx == 1 && dy == -1) {
+				dirList.push_front(DIRECTION_SOUTHWEST);
+			} else if (dx == -1 && dy == -1) {
+				dirList.push_front(DIRECTION_SOUTHEAST);
+			} else if (dx == 1) {
+				dirList.push_front(DIRECTION_WEST);
+			} else if (dx == -1) {
+				dirList.push_front(DIRECTION_EAST);
+			} else if (dy == 1) {
+				dirList.push_front(DIRECTION_NORTH);
+			} else if (dy == -1) {
+				dirList.push_front(DIRECTION_SOUTH);
+			}
+
+			found = found->parent;
+		}
+		return true;
+	} catch (const std::invalid_argument &e) {
+		SPDLOG_ERROR("An error occurred in the '[{}]' function: {}", __FUNCTION__, e.what());
+	} catch (const std::exception &e) {
+		SPDLOG_ERROR("An unexpected error occurred in the '[{}]' function: {}", __FUNCTION__, e.what());
 	}
-	return true;
 }
 
 // AStarNodes

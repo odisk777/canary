391a58cad5b64d8d2ad280e7c2c625f7b27b255e
odisk777 <65802862+odisk777@users.noreply.github.com>
2023-07-28 00:28:02 -0500

    Revert "fix: use executeQuery on update query (#1233)"
    This reverts commit 98087d012d72afdde814d1c0932772fc04fcc420.

--- src/io/iologindata.cpp
+++ src/io/iologindata.cpp
@@ -165,7 +165,7 @@ bool IOLoginData::preloadPlayer(Player* player, const std::string &name) {
 	if (creation == 0) {
 		query.str(std::string());
 		query << "UPDATE `accounts` SET `creation` = " << static_cast<uint32_t>(time(nullptr)) << " WHERE `id` = " << player->accountNumber;
-		db.executeQuery(query.str());
+		db.storeQuery(query.str());
 	}
 
 	// If the player has more premium days than he purchased, it means data existed before the loyalty system was implemented.
@@ -173,7 +173,7 @@ bool IOLoginData::preloadPlayer(Player* player, const std::string &name) {
 	if (premiumDays > premiumDaysPurchased) {
 		query.str(std::string());
 		query << "UPDATE `accounts` SET `premdays_purchased` = " << premiumDays << " WHERE `id` = " << player->accountNumber;
-		db.executeQuery(query.str());
+		db.storeQuery(query.str());
 	}
 
 	player->loyaltyPoints = static_cast<uint32_t>(std::ceil((static_cast<double>(time(nullptr) - creation)) / 86400)) * g_configManager().getNumber(LOYALTY_POINTS_PER_CREATION_DAY)
